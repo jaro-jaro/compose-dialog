4ddd41254100c5746060732dbe0c30313293b84b
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2023-10-18 18:39:26 +0200

    1.0.2

--- compose-dialog/build.gradle.kts
+++ compose-dialog/build.gradle.kts
@@ -11,7 +11,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.jaro-jaro"
             artifactId = "compose-dialog"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
